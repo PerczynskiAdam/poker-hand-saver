8ae3858aa0149d07f764a722b68b3dd44cd0dbb2
PerczynskiAdam <perczynskiadam@gmail.com>
2020-07-27 20:00:01 +0200

    Added navbar

--- hand/static/hand/js/hand_action.js
+++ hand/static/hand/js/hand_action.js
@@ -1,23 +1,45 @@
 const pre_btn = document.getElementById('go-to-pre')
 pre_btn.addEventListener('click', e => {
    e.preventDefault();
-   const hand_info = document.querySelectorAll('.hand-info');
+   let hand_info = document.querySelectorAll('.hand-info');
    hand_info.forEach(upd => {
       upd.classList.add('d-none');
    });
-   let pre_btn = document.getElementById('go-to-pre');
-   pre_btn.classList.add('d-none');
 
    let pre_info = document.querySelectorAll('.pre-info');
    pre_info.forEach(upd => {
       upd.classList.remove('d-none');
    });
 
-   let flop_btn = document.getElementById('go-to-flop');
-   flop_btn.classList.remove('d-none');
+   let flop = document.querySelectorAll('.flop-info');
+   flop.forEach(upd => {
+      upd.classList.add('d-none');
+   });
 
-   // let hand_info_btn_up = document.getElementById('hand-info-btn');
-   // hand_info_btn_up.classList.remove('d-none');
+   let turn = document.querySelectorAll('.turn-info');
+   turn.forEach(upd => {
+      upd.classList.add('d-none')
+   });
+
+   let river = document.querySelectorAll('.river-info');
+   river.forEach(upd => {
+      upd.classList.add('d-none')
+   });
+
+   let inf_btn = document.getElementById('go-to-info-li');
+   inf_btn.classList.remove('active');
+
+   let pre_btn = document.getElementById('go-to-pre-li');
+   pre_btn.classList.add('active');
+
+   let flop_btn = document.getElementById('go-to-flop-li');
+   flop_btn.classList.remove('active');
+
+   let turn_btn = document.getElementById('go-to-turn-li')
+   turn_btn.classList.remove('active');
+
+   let river_btn = document.getElementById('go-to-river-li');
+   river_btn.classList.remove('active');
 
 
 })
@@ -26,58 +48,134 @@ pre_btn.addEventListener('click', e => {
 const flop_btn = document.getElementById('go-to-flop')
 flop_btn.addEventListener('click', e => {
    e.preventDefault();
-   let flop = document.querySelectorAll('.flop-info');
-   flop.forEach(upd => {
-      upd.classList.remove('d-none');
+
+   let hand_info = document.querySelectorAll('.hand-info');
+   hand_info.forEach(upd => {
+      upd.classList.add('d-none');
    });
-   // let hand_info_btn_up = document.getElementById('hand-info-btn');
-   // hand_info_btn_up.classList.add('d-none');
 
    let pre_info = document.querySelectorAll('.pre-info');
    pre_info.forEach(upd => {
       upd.classList.add('d-none');
    });
-   // let pre_btn = document.getElementById('go-to-pre');
-   // pre_btn.classList.remove('d-none');
-   let turn_btn = document.getElementById('go-to-turn')
-   turn_btn.classList.remove('d-none')
 
-   let flop_btn = document.getElementById('go-to-flop');
-   flop_btn.classList.add('d-none');
+   let flop = document.querySelectorAll('.flop-info');
+   flop.forEach(upd => {
+      upd.classList.remove('d-none');
+   });
+
+   let turn = document.querySelectorAll('.turn-info');
+   turn.forEach(upd => {
+      upd.classList.add('d-none')
+   });
+
+   let river = document.querySelectorAll('.river-info');
+   river.forEach(upd => {
+      upd.classList.add('d-none')
+   });
+   let inf_btn = document.getElementById('go-to-info-li');
+   inf_btn.classList.remove('active');
+
+   let pre_btn = document.getElementById('go-to-pre-li');
+   pre_btn.classList.remove('active');
+
+   let flop_btn = document.getElementById('go-to-flop-li');
+   flop_btn.classList.add('active');
+
+   let turn_btn = document.getElementById('go-to-turn-li')
+   turn_btn.classList.remove('active');
+
+   let river_btn = document.getElementById('go-to-river-li');
+   river_btn.classList.remove('active');
+
 })
 
 const turn_btn = document.getElementById('go-to-turn')
 turn_btn.addEventListener('click', e => {
    e.preventDefault()
-   let turn = document.querySelectorAll('.turn-info');
-   turn.forEach(upd => {
-      upd.classList.remove('d-none')
+   let hand_info = document.querySelectorAll('.hand-info');
+   hand_info.forEach(upd => {
+      upd.classList.add('d-none');
    });
+
+   let pre_info = document.querySelectorAll('.pre-info');
+   pre_info.forEach(upd => {
+      upd.classList.add('d-none');
+   });
+
    let flop = document.querySelectorAll('.flop-info');
    flop.forEach(upd => {
       upd.classList.add('d-none');
    });
-   let river_btn = document.getElementById('go-to-river')
-   river_btn.classList.remove('d-none');
-   let turn_btn = document.getElementById('go-to-turn')
-   turn_btn.classList.add('d-none');
+
+   let turn = document.querySelectorAll('.turn-info');
+   turn.forEach(upd => {
+      upd.classList.remove('d-none')
+   });
+
+   let river = document.querySelectorAll('.river-info');
+   river.forEach(upd => {
+      upd.classList.add('d-none')
+   });
+
+   let inf_btn = document.getElementById('go-to-info-li');
+   inf_btn.classList.remove('active');
+
+   let pre_btn = document.getElementById('go-to-pre-li');
+   pre_btn.classList.remove('active');
+
+   let flop_btn = document.getElementById('go-to-flop-li');
+   flop_btn.classList.remove('active');
+
+   let turn_btn = document.getElementById('go-to-turn-li')
+   turn_btn.classList.add('active');
+
+   let river_btn = document.getElementById('go-to-river-li');
+   river_btn.classList.remove('active');
 })
 
 const river_btn = document.getElementById('go-to-river');
 river_btn.addEventListener('click', e => {
    e.preventDefault()
-   let river = document.querySelectorAll('.river-info');
-   river.forEach(upd => {
-      upd.classList.remove('d-none')
+   let hand_info = document.querySelectorAll('.hand-info');
+   hand_info.forEach(upd => {
+      upd.classList.add('d-none');
    });
+
+   let pre_info = document.querySelectorAll('.pre-info');
+   pre_info.forEach(upd => {
+      upd.classList.add('d-none');
+   });
+
+   let flop = document.querySelectorAll('.flop-info');
+   flop.forEach(upd => {
+      upd.classList.add('d-none');
+   });
+
    let turn = document.querySelectorAll('.turn-info')
    turn.forEach(upd => {
       upd.classList.add('d-none')
    });
 
-   let river_btn = document.getElementById('go-to-river');
-   river_btn.classList.add('d-none');
+   let river = document.querySelectorAll('.river-info');
+   river.forEach(upd => {
+      upd.classList.remove('d-none')
+   });
 
+   let inf_btn = document.getElementById('go-to-info-li');
+   inf_btn.classList.remove('active');
+
+   let pre_btn = document.getElementById('go-to-pre-li');
+   pre_btn.classList.remove('active');
+   
+   let flop_btn = document.getElementById('go-to-flop-li');
+   flop_btn.classList.remove('active');
+
+   let turn_btn = document.getElementById('go-to-turn-li')
+   turn_btn.classList.remove('active');
+
+   let river_btn = document.getElementById('go-to-river-li');
+   river_btn.classList.add('active');
 
 })
 
